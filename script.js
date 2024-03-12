eb3b1a21e1e1ea064d0173e90536afba8d1b08be
Monika Wilk <monikawilkpr@gmail.com>
2024-03-12 19:54:36 +0100

    INITIAL COMMIT

--- script.js
+++ script.js
@@ -2,57 +2,85 @@
   const tasks = [
     {
       content: "przeczytać notatki",
-      done: false,
-    },
-    {
-      content: "wypić wodę",
       done: true,
     },
+    {
+      content: "wypić sok",
+      done: false,
+    },
   ];
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+    render();
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+  };
+
+  const bindEvents = () => {
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index);
+      });
+    });
+  };
+
+  const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+  toggleDoneButtons.forEach((toggleDoneButton, index) => {
+    toggleDoneButton.addEventListener("click", () => {
+      toggleTaskDone(index);
+    });
+  });
 
   const render = () => {
     let htmlString = "";
 
     for (const task of tasks) {
       htmlString += `
-        <li style="text decoration:  line-through">
-      
-        
+        <li
+        ${task.done ? " style =\"text-decoration: line-through\"" : ""}>
+        <button class="js-done">Zrobione?</button>
+        <button class="js-remove">Usuń</button>
             ${task.content}
         </li>
         `;
     }
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    bindEvents();
   };
 
-  const addNewTask = (newTaskContent) => {
-    tasks.push({
-      content: newTaskContent,
-
-    });
-    render();
-  }
-  const init = () => {};
-  render();
-
-  const form = document.querySelector(".js-form");
-
-  form.addEventListener("sumbit", (event) => {
+  const onFormSumbit = (event) => {
     event.preventDefault();
 
     const newTaskContent = document.querySelector(".js-newTask").value.trim();
-    console.log(newTaskContent);
-
     if (newTaskContent === "") {
       return;
     }
 
     addNewTask(newTaskContent);
+  };
 
-   
-});
-        };
+  const init = () => {
+    render();
 
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSumbit);
+  };
   init();
 }
